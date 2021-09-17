d9a0e76767e3ea717ef73b33d994ee3c2b9d9cae
lverghese <loganverghese@gmail.com>
2021-09-17 19:34:21 -0400

    tag routes completed

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -42,14 +42,60 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(tagPostData => res.json(tagPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+        tag_name: req.body.tag_name
+    },
+    {
+      where: {
+          id: req.params.id
+      }
+    }
+)
+.then(tagPutChange => {
+    if (!tagPutChange) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+    }
+    res.json(tagPutChange);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+        id: req.params.id
+    }
+})
+.then(tagDeleteData => {
+    if (!tagDeleteData) {
+        res.status(404).json({ message: 'No tag with this id'});
+        return;
+    }
+    res.json(tagDeleteData);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 });
 
 module.exports = router;
